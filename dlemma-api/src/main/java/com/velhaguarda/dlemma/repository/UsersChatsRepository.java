b13f8cdf92177aad27af1dd4d1593d040323ec1e
Gil Martins <carlosgilmds@gmail.com>
2025-07-05 22:39:03 -0300

    Listar  e Remover Participantes

--- dlemma-api/src/main/java/com/velhaguarda/dlemma/repository/UsersChatsRepository.java
+++ dlemma-api/src/main/java/com/velhaguarda/dlemma/repository/UsersChatsRepository.java
@@ -2,7 +2,12 @@ package com.velhaguarda.dlemma.repository;
 
 import com.velhaguarda.dlemma.entity.UsersChats;
 import com.velhaguarda.dlemma.entity.UsersChatsId;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersChatsRepository extends JpaRepository<UsersChats, UsersChatsId> {
+     //listar todos os UsersChats de um determinado dilema
+    List<UsersChats> findByDilemma_IdDilemma(int dilemmaId);
 }
